ca06f4dc7bf2b39e2f786d582b6f99bc36560793
Hisoka-Kyo <319495298@qq.com>
2025-06-04 09:19:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,8 +29,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'AI宝库', url: '' },
-  HEO_HERO_CATEGORY_2: { title: '教学工具', url: '/tag/教学工具' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '教学工具', url: '/tag/工具' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
